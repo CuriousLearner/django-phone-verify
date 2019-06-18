5c652bd943729299612274d02d6ea7355b7ca51d
Sanyam Khurana <sanyam.khurana01@gmail.com>
2019-06-18 15:51:16 +0530

    Update to v0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-phone-verify',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='GPLv3',
