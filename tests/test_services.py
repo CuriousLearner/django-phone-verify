9c4b316c6cba661bfc1966427b8a9de2d7af2896
Sepehr Hasanabadi <mse.hasanabadi@gmail.com>
2019-12-29 11:22:59 +0330

    kavenegar api iranian OTP

--- tests/test_services.py
+++ tests/test_services.py
@@ -6,8 +6,7 @@ import pytest
 # phone_verify Stuff
 from phone_verify.services import PhoneVerificationService
 
-pytestmark = pytest.mark.django_db
-
+PYTESTMARK = pytest.mark.django_db
 
 def test_message_generation_and_sending_service(client, mocker):
     service = PhoneVerificationService(phone_number="+13478379634")
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,29 +1,3 @@
-KAVENEGAR_VERIFICATION: {
-    "BACKEND": "phone_verify.backends.kavenegar.KavenegarBackend",
-	"OPTIONS": {
-		"API_KEY": "fake",
-		"SENDER": "+14755292729"
-	},
-	"TOKEN_LENGTH": 6,
-	"MESSAGE": "Welcome to {app}! Please use security code {security_code} to proceed.",
-	"APP_NAME": "Phone Verify",
-	"SECURITY_CODE_EXPIRATION_TIME": 1,  # In seconds only
-	"VERIFY_SECURITY_CODE_ONLY_ONCE": False,
-}
-TWILIO_VERIFICATION: {
-	"BACKEND": "phone_verify.backends.twilio.TwilioBackend",
-	"OPTIONS": {
-		"SID": "fake",
-		"SECRET": "fake",
-		"FROM": "+14755292729",
-		"SANDBOX_TOKEN": "123456",
-	},
-	"TOKEN_LENGTH": 6,
-	"MESSAGE": "Welcome to {app}! Please use security code {security_code} to proceed.",
-	"APP_NAME": "Phone Verify",
-	"SECURITY_CODE_EXPIRATION_TIME": 1,  # In seconds only
-	"VERIFY_SECURITY_CODE_ONLY_ONCE": False,
-}
 DJANGO_SETTINGS = {
     "SECRET_KEY": "change-me-later",
     "DATABASES": {"default": {"ENGINE": "django.db.backends.sqlite3"}},
@@ -33,6 +7,32 @@ DJANGO_SETTINGS = {
         "django.contrib.contenttypes",
         "phone_verify",
     ],
-	# PHONE VERIFICATION
-    "PHONE_VERIFICATION": KAVENEGAR_VERIFICATION
+    # PHONE VERIFICATION
+    "PHONE_VERIFICATION": {
+        "BACKEND": "phone_verify.backends.twilio.TwilioBackend",
+        "OPTIONS": {
+            "SID": "fake",
+            "SECRET": "fake",
+            "FROM": "+14755292729",
+            "SANDBOX_TOKEN": "123456",
+        },
+        "TOKEN_LENGTH": 6,
+        "MESSAGE": "Welcome to {app}! Please use security code {security_code} to proceed.",
+        "APP_NAME": "Phone Verify",
+        "SECURITY_CODE_EXPIRATION_TIME": 1,  # In seconds only
+        "VERIFY_SECURITY_CODE_ONLY_ONCE": False,
+    },
+	# KAVENEGAR VERIFICATION
+	"KAVENEGAR_VERIFICATION": {
+		"BACKEND": "phone_verify.backends.kavenegar.KavenegarBackend",
+		"OPTIONS": {
+			"API_KEY": "fake",
+			"SENDER": "+14755292729"
+		},
+		"TOKEN_LENGTH": 6,
+		"MESSAGE": "Welcome to {app}! Please use security code {security_code} to proceed.",
+		"APP_NAME": "Phone Verify",
+		"SECURITY_CODE_EXPIRATION_TIME": 1,  # In seconds only
+		"VERIFY_SECURITY_CODE_ONLY_ONCE": False,
+	}
 }
