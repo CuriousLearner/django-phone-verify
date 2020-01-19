5596a2e376f1518fc1ad88eedfb539da3de15618
Sepehr Hasanabadi <mse.hasanabadi@gmail.com>
2020-01-19 16:49:47 +0330

    feat(docs): Add Iranian sending message platform (Kavenegar)

--- phone_verify/serializers.py
+++ phone_verify/serializers.py
@@ -15,35 +15,35 @@ logger = logging.getLogger(__name__)
 
 
 class PhoneSerializer(serializers.Serializer):
-    phone_number = PhoneNumberField()
+	phone_number = PhoneNumberField()
 
 
 class SMSVerificationSerializer(serializers.Serializer):
-    phone_number = PhoneNumberField(required=True)
-    session_token = serializers.CharField(required=True)
-    security_code = serializers.CharField(required=True)
+	phone_number = PhoneNumberField(required=True)
+	session_token = serializers.CharField(required=True)
+	security_code = serializers.CharField(required=True)
 
-    def validate(self, attrs):
-        attrs = super().validate(attrs)
-        phone_number = attrs.get("phone_number", None)
-        security_code, session_token = (
-            attrs.get("security_code", None),
-            attrs.get("session_token", None),
-        )
-        backend = get_sms_backend(phone_number=phone_number)
-        verification, token_validatation = backend.validate_security_code(
-            security_code=security_code,
-            phone_number=phone_number,
-            session_token=session_token,
-        )
+	def validate(self, attrs):
+		attrs = super().validate(attrs)
+		phone_number = attrs.get("phone_number", None)
+		security_code, session_token = (
+			attrs.get("security_code", None),
+			attrs.get("session_token", None),
+		)
+		backend = get_sms_backend(phone_number=phone_number)
+		verification, token_validatation = backend.validate_security_code(
+			security_code=security_code,
+			phone_number=phone_number,
+			session_token=session_token,
+		)
 
-        if verification is None:
-            raise serializers.ValidationError(_("Security code is not valid"))
-        elif token_validatation == backend.SESSION_TOKEN_INVALID:
-            raise serializers.ValidationError(_("Session Token mis-match"))
-        elif token_validatation == backend.SECURITY_CODE_EXPIRED:
-            raise serializers.ValidationError(_("Security code has expired"))
-        elif token_validatation == backend.SECURITY_CODE_VERIFIED:
-            raise serializers.ValidationError(_("Security code is already verified"))
+		if verification is None:
+			raise serializers.ValidationError(_("Security code is not valid"))
+		elif token_validatation == backend.SESSION_TOKEN_INVALID:
+			raise serializers.ValidationError(_("Session Token mis-match"))
+		elif token_validatation == backend.SECURITY_CODE_EXPIRED:
+			raise serializers.ValidationError(_("Security code has expired"))
+		elif token_validatation == backend.SECURITY_CODE_VERIFIED:
+			raise serializers.ValidationError(_("Security code is already verified"))
 
-        return attrs
+		return attrs
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,38 +1,39 @@
 DJANGO_SETTINGS = {
-    "SECRET_KEY": "change-me-later",
-    "DATABASES": {"default": {"ENGINE": "django.db.backends.sqlite3"}},
-    "ROOT_URLCONF": "phone_verify.urls",
-    "INSTALLED_APPS": [
-        "django.contrib.auth",
-        "django.contrib.contenttypes",
-        "phone_verify",
-    ],
-    # PHONE VERIFICATION
-    "PHONE_VERIFICATION": {
-        "BACKEND": "phone_verify.backends.twilio.TwilioBackend",
-        "OPTIONS": {
-            "SID": "fake",
-            "SECRET": "fake",
-            "FROM": "+14755292729",
-            "SANDBOX_TOKEN": "123456",
-        },
-        "TOKEN_LENGTH": 6,
-        "MESSAGE": "Welcome to {app}! Please use security code {security_code} to proceed.",
-        "APP_NAME": "Phone Verify",
-        "SECURITY_CODE_EXPIRATION_TIME": 1,  # In seconds only
-        "VERIFY_SECURITY_CODE_ONLY_ONCE": False,
-    },
-	# KAVENEGAR VERIFICATION
-	"KAVENEGAR_VERIFICATION": {
-		"BACKEND": "phone_verify.backends.kavenegar.KavenegarBackend",
-		"OPTIONS": {
-			"API_KEY": "fake",
-			"SENDER": "+14755292729"
+	"SECRET_KEY": "change-me-later",
+	"DATABASES": {"default": {"ENGINE": "django.db.backends.sqlite3"}},
+	"ROOT_URLCONF": "phone_verify.urls",
+	"INSTALLED_APPS": [
+		"django.contrib.auth",
+		"django.contrib.contenttypes",
+		"phone_verify",
+	],
+	# PHONE VERIFICATION
+	"PHONE_VERIFICATION": {
+		"TWILIO": {
+			"BACKEND": "phone_verify.backends.twilio.TwilioBackend",
+			"OPTIONS": {
+				"SID": "fake",
+				"SECRET": "fake",
+				"FROM": "+14755292729",
+				"SANDBOX_TOKEN": "123456",
+			},
+			"TOKEN_LENGTH": 6,
+			"MESSAGE": "Welcome to {app}! Please use security code {security_code} to proceed.",
+			"APP_NAME": "Phone Verify",
+			"SECURITY_CODE_EXPIRATION_TIME": 1,  # In seconds only
+			"VERIFY_SECURITY_CODE_ONLY_ONCE": False,
 		},
-		"TOKEN_LENGTH": 6,
-		"MESSAGE": "Welcome to {app}! Please use security code {security_code} to proceed.",
-		"APP_NAME": "Phone Verify",
-		"SECURITY_CODE_EXPIRATION_TIME": 1,  # In seconds only
-		"VERIFY_SECURITY_CODE_ONLY_ONCE": False,
-	}
+		"DEFAULT": {
+			"BACKEND": "phone_verify.backends.kavenegar.KavenegarBackend",
+			"OPTIONS": {
+				"API_KEY": "fake",
+				"SENDER": "+14755292729"
+			},
+			"TOKEN_LENGTH": 6,
+			"MESSAGE": "Welcome to {app}! Please use security code {security_code} to proceed.",
+			"APP_NAME": "Phone Verify",
+			"SECURITY_CODE_EXPIRATION_TIME": 1,  # In seconds only
+			"VERIFY_SECURITY_CODE_ONLY_ONCE": False,
+		}
+	},
 }
