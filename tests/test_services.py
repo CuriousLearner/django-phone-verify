5a1aad2ef7c35cd90e77910bc13a613748c05ad3
Prashant Sharma <31796326+gutsytechster@users.noreply.github.com>
2020-02-29 14:51:06 +0530

    tests(*): Use parametrize fixtures and refactor tests (#40)
    Co-authored-by: Prashant Sharma <prashantsharma161198@gmail.com>
    
    Co-authored-by: Sanyam Khurana <8039608+CuriousLearner@users.noreply.github.com>

--- tests/test_services.py
+++ tests/test_services.py
@@ -2,6 +2,7 @@
 
 # Third Party Stuff
 import pytest
+from django.test import override_settings
 
 # phone_verify Stuff
 from phone_verify.services import PhoneVerificationService
@@ -9,9 +10,11 @@ from phone_verify.services import PhoneVerificationService
 pytestmark = pytest.mark.django_db
 
 
-def test_message_generation_and_sending_service(client, mocker):
-    service = PhoneVerificationService(phone_number="+13478379634")
-    twilio_api = mocker.patch("phone_verify.backends.twilio.TwilioBackend.send_sms")
-    service.send_verification("+13478379634", "123456")
+def test_message_generation_and_sending_service(client, mocker, backend):
+    with override_settings(PHONE_VERIFICATION=backend):
+        service = PhoneVerificationService(phone_number="+13478379634")
+        backend_service = backend.get("BACKEND")
+        mock_api = mocker.patch(f"{backend_service}.send_sms")
+        service.send_verification("+13478379634", "123456")
 
-    assert twilio_api.called
+        assert mock_api.called
